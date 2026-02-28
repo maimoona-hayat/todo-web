4d4c639f8114f8b1bd5630b2946352ee61e3adb8
maimoona-hayat <sadozaihayat@gmail.com>
2026-02-28 20:21:10 +0500

    use axios interceptor

---
+++ src/axios/api.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.VITE_API_URL ;
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+});
+
+// Request Interceptor
+instance.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      req.headers["x-auth-token"] = token;
+    }
+
+    return req;
+  },
+  (error) => {
+    console.error("Request error:", error);
+    return Promise.reject(error); 
+  }
+);
+
+
+// Response Interceptor
+instance.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    console.error("Response error:", error);
+
+    if (error.response?.status === 401) {
+      // Token expired or invalid
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error); 
+  }
+);
+
+// Auth
+export const registerUser = (formData) =>
+  instance.post("/api/v1/user/register", formData);
+
+export const loginUser = (data) =>
+  instance.post("/api/v1/user/login", data);
+
+// Todos
+export const getTodos = (page = 1) =>
+  instance.get(`/api/v1/todo?page=${page}`);
+
+export const createTodo = (data) =>
+  instance.post("/api/v1/todo", data);
+
+export const updateTodo = (id, data) =>
+  instance.put(`/api/v1/todo/${id}`, data);
+
+export const deleteTodo = (id) =>
+  instance.delete(`/api/v1/todo/${id}`);
